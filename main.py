b4cbdf91e02df3e5d5aa1abd346e67da618bc629
Muhammed Ali <muhamzyali@gmail.com>
2023-08-31 14:56:58 +0100

    fix lint problem

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from flask import Flask
 
-app = Flask(__name__) #create an instance of the Flask class you just imported
+# create an instance of the Flask class you just imported
+app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def main():
     # display a string in the home page
     return "Welcome to my Flask App"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
--- test_main.py
+++ test_main.py
@@ -1,7 +1,10 @@
 from main import app
 
-def test_main():
-    response = app.test_client().get('/') # Creates a test client for this application.
 
-    assert response.status_code == 400 # assert the stutus code of the page('/') is 200
-    assert response.data == b'Welcome to my Flask App' # assert the return statement ton the page
+def test_main():
+    # Creates a test client for this application.
+    response = app.test_client().get("/")
+    # assert the status code of the page('/') is 200
+    assert response.status_code == 400
+    # assert the return statement ton the page
+    assert response.data == b"Welcome to my Flask App"

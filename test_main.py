10e0fc17f9546c43e17ed4b16592ca4859a110af
Muhammed Ali <muhamzyali@gmail.com>
2023-08-31 15:00:26 +0100

    fix unit test

--- test_main.py
+++ test_main.py
@@ -5,6 +5,6 @@ def test_main():
     # Creates a test client for this application.
     response = app.test_client().get("/")
     # assert the status code of the page('/') is 200
-    assert response.status_code == 400
+    assert response.status_code == 200
     # assert the return statement ton the page
     assert response.data == b"Welcome to my Flask App"
